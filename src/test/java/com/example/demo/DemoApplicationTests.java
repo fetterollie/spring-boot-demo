cd43b014c3a136e55c72aa5ca8a65035cb6710b0
fetterollie <jonathan.fetterolf@gmail.com>
2023-08-08 11:41:26 -0400

    more work to set up hibernate/jpa

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,41 @@
 package com.example.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.web.ServerProperties.Reactive.Session;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	private SessionFactory sessionFactory;
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+					.configure() // configures settings from hibernate.cfg.xml
+					.build();
+		try {
+			sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory,
+			// but we had trouble building the SessionFactory so destroy it manually.
+			StandardServiceRegistryBuilder.destroy( registry );
+		}
+	}
+
+	// @AfterEach
+
 }
 
-// @Test
-// void save_my_first_object_to_the_db(){
-// 	try (Session session = sessionFactory.openSession()) {
-// 		session.beginTransaction();
-
-// 		// save the stuff
-
-// 		session.getTransaction().commit();
-// 	}
-// }
