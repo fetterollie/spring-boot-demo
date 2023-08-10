82b53ab05d95e48f3d6a7a876f8800c76a8cac6a
fetterollie <jonathan.fetterolf@gmail.com>
2023-08-10 10:15:55 -0400

    added connection to front-end

--- src/main/java/com/example/demo/CarController.java
+++ src/main/java/com/example/demo/CarController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 // import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/v1")
 public class CarController {
 
