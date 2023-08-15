25b3e715d9fc50ab9cae6f18cbff8ff4d6bbaae9
fetterollie <jonathan.fetterolf@gmail.com>
2023-08-15 11:40:40 -0400

    cleaning unused code

--- src/main/java/com/example/demo/CarController.java
+++ src/main/java/com/example/demo/CarController.java
@@ -21,12 +21,13 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
+// rest controller annoataion
 @RestController
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/v1")
 public class CarController {
 
+    // Autowired annotation to enable repository methods
     @Autowired
     private CarRepository carRepository;
 
@@ -87,74 +88,3 @@ public class CarController {
 
     }
 }
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.validation.annotation.Validated;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.server.ResponseStatusException;
-
-// // annotation small change to test git
-// @RestController
-// @RequestMapping("/car")
-// public class CarController {
-
-//     @Autowired
-//     private CarsService carsService;
-    
-//     Car car;
-
-//     @GetMapping("/")
-//     public String helloCar() {
-//         return "Hello Car!";
-//     }
-
-//     @GetMapping("{car_id}")
-//     public Car get(@PathVariable String car_id) {
-//         if (car == null) {
-//             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-//         } else {
-//             return carsService.get(car_id);
-//         }
-//     }
-
-//     @GetMapping("/cars") 
-//     public Iterable<Car> get() {
-//         return carsService.get();
-//     }
-
-//     @DeleteMapping("/{car_id}")
-//     public void deleteCar(@PathVariable String car_id) {
-//         if (car == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-//         this.car = carsService.remove(car_id);
-//     }
-
-//     @PostMapping
-//     public ResponseEntity<String> createCarDetails(@RequestBody @Validated Car car) {
-        
-//         this.car = car;
-
-//         if (car.getCar_id() == null) throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE);
-
-//         // jon to-do: fix car service to work with Integer car_id
-//         carsService.put(car.getCar_id(), car);
-       
-//         return new ResponseEntity<String> (
-//             String.format(
-//                 "Car creation successful!\ncar_id: %s\nmake: %s\nmodel: %s", 
-//                 car.getCar_id(), 
-//                 car.getMake(),
-//                 car.getModel()
-//             ),
-//             HttpStatus.CREATED
-//         );
-
-//     }
-// }
